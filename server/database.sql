ee1198466ee844d771e5628b1e279eea05bf9196
Nachiketha KR <arclops@gmail.com>
2023-10-29 12:27:54 +0530

    Added login, register server logic

--- server/database.sql
+++ server/database.sql
@@ -7,13 +7,15 @@ create table user (
     uid int not null auto_increment,
     fname varchar(15) not null,
     lname varchar(15) not null,
-    email varchar(50) not null,
+    email varchar(50),
     password varchar(64) not null,
     hashedpassword varchar(128) not null,
-    phone int(15) not null,
+    phone int(15),
     role varchar(10) not null check (role in ('admin', 'farmer', 'customer')),
     regdate datetime not null,
     lastlogin datetime,
     primary key (uid),
     unique (email),
+    unique (phone),
+    CONSTRAINT email_or_phone CHECK ((email IS NOT NULL AND phone IS NULL) OR (email IS NULL AND phone IS NOT NULL)) OR (email is NOT NULL AND phone IS NOT NULL)
 )
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 const fs = require("fs");
+const bcrypt = require("bcrypt");
 
 // //Create Hashed
 // const bcrypt = require('bcrypt');
@@ -35,7 +36,92 @@ app.use(cors());
 app.use(express.json());
 
 //Registration Routes
+app.get("/login", (req, res) => {
+    try{
+        const {email,password,phone} = req.body;
+        if (email && password) {
+            pool.query('SELECT * FROM users WHERE email = $1', [email], (error, results) => {
+                if (results.rows.length > 0) {
+                    const user = results.rows[0];
+                    bcrypt.compare(password, user.password, function(err, result) {
+                        if (result) {
+                            res.json(user);
+                        }
+                        else {
+                            res.status(401).send('Incorrect password');
+                        }
+                    })
+                } else {
+                    res.status(401).send('User not found, check entered email');
+                }
+            });
+        }
+        if (phone && password) {
+            pool.query('SELECT * FROM users WHERE phone = $1', [phone], (error, results) => {
+                if (results.rows.length > 0) {
+                    const user = results.rows[0];
+                    bcrypt.compare(password, user.password, function(err, result) {
+                        if (result) {
+                            res.json(user);
+                        }
+                        else {
+                            res.status(401).send('Incorrect password');
+                        }
+                    })
+                } else {
+                    res.status(401).send('User not found, check entered phone');
+                }
+            })
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+})
 
 app.post("/register", async (req, res) => {
-    
+    try {
+        const { email, password, phone } = req.body;
+        if (email && password) {
+            if(!phone) phone = null;
+            bcrypt.genSalt(10, function(err, salt) {
+                bcrypt.hash(password, salt, function(err, hash) {
+                    const hashedPassword = hash;
+                    pool.query(
+                        "INSERT INTO users (email, password,phone ) VALUES ($1, $2) RETURNING *",
+                        [email, hashedPassword, phone],
+                        (error, results) => {
+                            if (error) {
+                                throw error;
+                            }
+                            res.status(201).send(`User added with ID: ${results.rows[0].id}`);
+                        }
+                    );
+                });
+              });
+        }
+        if (phone && password) {
+            if(!email) email = null;
+            bcrypt.genSalt(saltRounds, function(err, salt) {
+                bcrypt.hash(plainPassword, salt, function(err, hash) {
+                    const hashedPassword = hash;
+                    pool.query(
+                        "INSERT INTO users (phone, password, email) VALUES ($1, $2) RETURNING *",
+                        [phone, hashedPassword, email],
+                        (error, results) => {
+                            if (error) {
+                                throw error;
+                            }
+                            res.status(201).send(`User added with ID: ${results.rows[0].id}`);
+                        }
+                    );
+                });
+              });
+        }
+    } catch (error) {
+        console.log(error.message);
+    }
+});
+
+app.listen(5000, () => {
+    console.log("server has started on port 5000");
 })
