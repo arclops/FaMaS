749b5a0e3f10983bb3377a628c79a40bc4ddd4c0
Nachiketha KR <arclops@gmail.com>
2023-11-28 21:22:58 +0530

    Base functions defined

--- server/database.sql
+++ server/database.sql
@@ -8,6 +8,24 @@ ALTER DATABASE postgres SET timezone TO 'Asia/Kolkata';
 -- To restart uid from 1, use the following command
 -- ALTER SEQUENCE users_uid_seq RESTART WITH 1;
 
+-- Farmer Dashboard tables
+
+CREATE TABLE farmers (
+    fid SERIAL PRIMARY KEY,
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    gender CHAR(1) CHECK (gender IN ('M', 'F', 'O')),
+    dob DATE NOT NULL,
+    phone VARCHAR(15) NOT NULL,
+    email VARCHAR(255),
+    address TEXT,
+    aadhar VARCHAR(12) UNIQUE NOT NULL,
+    pan VARCHAR(10) UNIQUE,
+    status VARCHAR(10) NOT NULL CHECK (status IN ('active', 'inactive', 'banned')),
+);
+
+-- Admin Dashboard tables
+
 CREATE TABLE users (
     uid SERIAL PRIMARY KEY,
     fname VARCHAR(15) NOT NULL,
@@ -30,3 +48,4 @@ create table serverlogs (
 	logs varchar(250) not null,
 	datetime timestamp not null
 );
+
