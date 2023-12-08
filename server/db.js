4f617d23e47cd7d3f0228ac7d2f434a71990d6fb
Nachiketha KR <arclops@gmail.com>
2023-12-09 00:14:47 +0530

    db.js

--- server/db.js
+++ server/db.js
@@ -2,11 +2,7 @@ const Pool = require("pg").Pool;
 require("dotenv").config();
 
 const pool = new Pool({
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASS,
-    host: process.env.POSTGRES_URL,
-    port: 5432,
-    database: "FaMaS"
+    connectionString: process.env.POSTGRES_URL
 });
 
 module.exports=pool;
