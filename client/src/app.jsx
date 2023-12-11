75d144fd1e55545db4cd78023c70f4242b3f5fd3
Nachiketha KR <arclops@gmail.com>
2023-12-11 12:23:49 +0530

    path fix #1.3

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 
-import '../global.css';
+import './global.css';
 import Router from "./routes/sections";
 import ThemeProvider from '../theme/index.j';
 import { useScrollToTop } from '../hooks/use-scroll-to-top.j';
