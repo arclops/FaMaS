cd76c102e1a80783a13c76d2b4dd9f541ae6a9e0
Nachiketha KR <arclops@gmail.com>
2023-12-11 12:16:03 +0530

    path fix #1.1

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import path from 'path';
+// import path from 'path';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 import react from '@vitejs/plugin-react-swc';
@@ -13,22 +13,6 @@ export default defineConfig({
       },
     }),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@hooks': path.resolve(__dirname, './src/hooks'),
-      '@utils': path.resolve(__dirname, './src/utils'),
-      '@images': path.resolve(__dirname, './src/images'),
-      '@layouts': path.resolve(__dirname, './src/layouts'),
-      '@pages': path.resolve(__dirname, './src/pages'),
-      '@routes': path.resolve(__dirname, './src/routes'),
-      '@sections': path.resolve(__dirname, './src/sections'),
-      '@theme': path.resolve(__dirname, './src/theme'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@_mock': path.resolve(__dirname, './src/_mock'),
-    }
-  },
   server: {
     port: 3030,
   },
